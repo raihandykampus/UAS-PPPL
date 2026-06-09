35cc22f4725be1c500a7bff04e8bcad3e0c286da
fatehhhh26 <brilianfatih52@gmail.com>
2026-06-09 15:41:47 +0700

    setup admin penjadwalan cucumber test

--- src/test/java/org/example/runners/RunTest.java
+++ src/test/java/org/example/runners/RunTest.java
@@ -1,12 +1,20 @@
 package org.example.runners;
 
-import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import io.cucumber.junit.platform.engine.Constants;
+
 @Suite
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(
+        key = Constants.GLUE_PROPERTY_NAME,
+        value = "org.example.steps"
+)
+@ConfigurationParameter(
+        key = Constants.PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json"
+)
 public class RunTest {
 }
---
+++ src/test/resources/features/admin_penjadwalan.feature
@@ -0,0 +1,6 @@
+Feature: Admin Penjadwalan
+
+  Scenario: Admin membuka halaman penjadwalan
+    Given admin sudah berada di halaman penjadwalan
+    When admin melihat form penjadwalan
+    Then form penjadwalan berhasil ditampilkan
